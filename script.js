7bd683596067bd484bba35905ba84cbc08b49b34
NIKITH KUMAR N <nikithkumarniki160698@gmail.com>
2024-03-16 19:57:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 //your JS code here. If required.
+
 function mapletters (word) {
 	const map = {};
 	
 	for (let i = 0; i < word.length; i++) {
 			const letter = word[i];
-			if(!map[letter]) {
+			if (!map[letter]) {
 				map[letter] = [i];
 			} else {
 				map[letter].push(i);
