76d4a30c19d531db85547ff17fbafe72a2303a84
NIKITH KUMAR N <nikithkumarniki160698@gmail.com>
2024-03-16 19:53:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+function mapletters(word){
+	const lettermap={};
+	for(let i=0;i<word.length;i++)
+		{
+			const letter=word[i];
+			if(!lettermap[letter]){
+				lettermap[letter]=[i];
+			}
+			else{
+				lettermap[letter].push(i);
+			}
+		}
+	return lettermap;
+}
+console.log(mapletters("dodo"));
